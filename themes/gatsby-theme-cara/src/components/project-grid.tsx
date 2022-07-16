2bdb0aa21e9e5d068a9dfb4a6066b93d66811c83
Orhan Sönmez <orhnsnmz@gmail.com>
2022-07-16 14:21:41 +0200

    Add `ProjectGrid` component

---
+++ themes/gatsby-theme-cara/src/components/project-grid.tsx
@@ -0,0 +1,22 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+
+type ProjectGridProps = {
+  children: React.ReactNode;
+};
+
+const ProjectGrid = ({ children }: ProjectGridProps) => (
+  <div
+    sx={{
+      flex: 1,
+      display: `grid`,
+      gridGap: [4, 4, 4, 5],
+      gridTemplateColumns: [`1fr`, `1fr`, `repeat(2, 1fr)`],
+      h2: { gridColumn: `-1/1`, color: `white !important` },
+    }}
+  >
+    {children}
+  </div>
+);
+
+export default ProjectGrid;
--- themes/gatsby-theme-cara/src/gatsby-plugin-theme-ui/components.js
+++ themes/gatsby-theme-cara/src/gatsby-plugin-theme-ui/components.js
@@ -1,13 +1,18 @@
 import * as React from "react"
 import ProjectCard from "../components/project-card"
+import ProjectGrid from "../components/project-grid";
 
 const components = {
+  // eslint-disable-next-line react/display-name
+  ProjectGrid: ({ children, ...props }) => (
+    <ProjectGrid {...props}>{children}</ProjectGrid>
+  ),
   // eslint-disable-next-line react/display-name
   ProjectCard: ({ link, title, bg, children, ...props }) => (
     <ProjectCard {...props} link={link} title={title} bg={bg}>
       {children}
     </ProjectCard>
   ),
-}
+};
 
 export default components
