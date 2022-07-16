03e9b447fc2441a953f1fa7addf9d8946987462c
Orhan Sönmez <orhnsnmz@gmail.com>
2022-07-16 22:58:02 +0200

    Add link to `mobile.css` stylesheet to `head` via `Helmet`

--- themes/gatsby-theme-cara/src/templates/cara.tsx
+++ themes/gatsby-theme-cara/src/templates/cara.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Parallax } from "@react-spring/parallax"
+import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 import Projects from "../components/projects"
@@ -8,6 +9,10 @@ import Contact from "../components/contact"
 
 const Cara = () => (
   <Layout>
+    <Helmet>
+      <link rel="stylesheet" href="mobile.css" />
+    </Helmet>
+
     <Parallax pages={5}>
       <Hero offset={0} factor={1} />
       <Projects offset={1} factor={2} />
