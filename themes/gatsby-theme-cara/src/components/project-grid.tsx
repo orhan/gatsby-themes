bed85752e16e1832939e7421b7fda7ae8cc41176
Orhan Sönmez <orhnsnmz@gmail.com>
2022-07-16 17:30:21 +0200

    Also allow `ProjectGrid` to have extra styling prop

--- themes/gatsby-theme-cara/src/components/project-grid.tsx
+++ themes/gatsby-theme-cara/src/components/project-grid.tsx
@@ -1,23 +1,27 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui";
+import { jsx } from "theme-ui"
 
 type ProjectGridProps = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+  style: any
+}
 
-const ProjectGrid = ({ children }: ProjectGridProps) => (
+const ProjectGrid = ({ children, style }: ProjectGridProps) => (
   <div
     sx={{
-      flex: 1,
-      display: `grid`,
-      gridGap: [4, 4, 4, 5],
-      gridTemplateColumns: [`1fr`, `1fr`, `1fr`, `repeat(3, 1fr)`],
-      justifyItems: `center`,
-      h2: { gridColumn: `-1/1`, color: `white !important` },
+      ...{
+        flex: 1,
+        display: `grid`,
+        gridGap: [4, 4, 4, 5],
+        gridTemplateColumns: [`1fr`, `1fr`, `1fr`, `repeat(3, 1fr)`],
+        justifyItems: `center`,
+        h2: { gridColumn: `-1/1`, color: `white !important` },
+      },
+      ...style,
     }}
   >
     {children}
   </div>
-);
+)
 
-export default ProjectGrid;
+export default ProjectGrid
